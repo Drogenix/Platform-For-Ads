b4ea4963cb8982d390347e96ed3a1b02d7650daf
GundONE <ilyaroshevich@gmail.com>
2023-03-24 15:56:07 +0300

    Вынес header в отдельный компонент, вставил его в app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {HeaderComponent} from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   imports: [
+    HeaderComponent,
     RouterOutlet
   ],
   standalone: true
